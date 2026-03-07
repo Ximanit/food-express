0bb2481e69783e572968ab1b650d6a2de1814f6d
Ximanit <122241859+Ximanit@users.noreply.github.com>
2026-03-08 01:18:16 +1000

    метрика

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://food-express-nsof.onrender.com/api", // или ваш продакшн URL
+  baseURL: "https://food-express-nsof.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
